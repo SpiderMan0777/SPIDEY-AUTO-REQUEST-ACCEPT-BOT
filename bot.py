9661e7a69a854f58d6eba77865200c6ab451bcaa
SpiderMan0777 <spideyofficial777@gmail.com>
2025-03-29 12:26:06 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -754,10 +754,12 @@ async def help_command(client, message):
         
         
 if __name__ == "__main__":
-    logger.info(script.LOGO_MSG)
+    logger.info(LOGO_MSG)
+    logger.info("SpideyCinemax Bot is running...")
     from threading import Thread
     Thread(target=lambda: app.run(host="0.0.0.0", port=8080)).start()
     bot.run()
+
 
 
 # Don't Remove Credit @spideyofficial777
