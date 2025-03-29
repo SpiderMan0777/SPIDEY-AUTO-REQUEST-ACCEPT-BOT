4ca436248c40a11295fbfb6d1e10355098279aaf
SpiderMan0777 <spideyofficial777@gmail.com>
2025-03-29 20:47:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "28519661"))
     API_HASH = getenv("API_HASH", "d47c74c8a596fd3048955b322304109d")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7785883068:AAGXu93ANNIMEy02MvYmV3LnWuSSxyl1sls")
     
     CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "-1001959922658,-1002470391435,-1002433552221").split(","))) # for the multiple forcesub
     
