e2ac10dce8bf2c72e2c540a48d5cce5f3e86869a
SpiderMan0777 <spideyofficial777@gmail.com>
2025-03-29 20:31:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -755,7 +755,7 @@ async def help_command(client, message):
         
         
 if __name__ == "__main__":
-    logger.info(LOGO_MSG)
+    logger.info(script.LOGO_MSG)
     logger.info("SpideyCinemax Bot is running...")
     from threading import Thread
     Thread(target=lambda: app.run(host="0.0.0.0", port=8080)).start()
